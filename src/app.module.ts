91512c83fcebcc54a892e295f0643468204b260a
Miikka Kivinen <miikka.kivinen@solita.fi>
2023-04-06 14:14:58 +0300

    db and users resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersModule } from './users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.PG_HOST,
+      port: +process.env.PG_PORT,
+      username: process.env.PG_USER,
+      password: process.env.PG_PASSWORD,
+      database: process.env.PG_DB,
+      autoLoadEntities: true,
+      synchronize: true,
+}),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
